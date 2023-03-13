44fb62a409c1ae369f51f997c8dae63d2b1b8d57
joeren2010 <sksports2002@gmail.com>
2023-03-13 06:15:07 -0600

    added ip-addy file, aws ip-addy and build this proj

---
+++ src/app/IpAddress.ts
@@ -0,0 +1,3 @@
+export function IpAddress() {
+  return '34.228.113.20'; //public ipv4 addy from aws' ec2 instance
+}
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from './product';
+import { IpAddress } from './IpAddress';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  baseUrl: string = 'http://localhost:9090/products';
+  baseUrl: string = 'http://34.228.113.20:9090/products'; //for aws - you enter ip addy in each service files
+  // baseUrl: string = `http://${IpAddress}:9090/products`;       //for aws - you enter ip addy in only one place
+  // baseUrl: string = 'http://localhost:9090/products';
   constructor(public http: HttpClient) {}
 
   loadAllProducts(): Observable<Product[]> {
